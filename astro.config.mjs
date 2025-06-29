2ee826dadbafa504b7275dee240f7296483706a3
jx06t <50313tjx06@gmail.com>
2025-06-29 19:10:17 +0800

    og-image?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: process.env.PUBLIC_SITE || 'http://localhost:4321',
+});
--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,11 +1,11 @@
 import ogImageSrc from "@images/branding/icon.svg";
 
 export const SITE = {
-  title: "Club.ck",
+  title: "CKClubHub",
   tagline: "一個整合了標籤系統以及收藏功能的建中社團線上資料庫",
   description: "一個整合了標籤系統以及收藏功能的建中社團線上資料庫",
   description_short: "一個整合了標籤系統以及收藏功能的建中社團線上資料庫",
-  url: "https://club.cksc.tw",
+  url: "https://ck-club-git-dev-jx06ts-projects.vercel.app/",
   author: "https://github.com/jx06T",
 };
 
