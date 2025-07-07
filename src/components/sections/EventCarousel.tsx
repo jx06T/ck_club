b20ee243e78490bf915e30bb6912c7b9e92c5e26
jx06t <50313tjx06@gmail.com>
2025-07-07 18:16:45 +0800

    輪播詳細訊息互動

--- src/components/sections/EventCarousel.tsx
+++ src/components/sections/EventCarousel.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, Fragment, useRef } from 'react';
-import { Calendar, SquareArrowOutUpRight, ChevronDown, ChevronUp } from "lucide-react"
+import { Calendar, SquareArrowOutUpRight, ChevronRight, ChevronLeft } from "lucide-react"
 
 export interface Slide {
     image: { src: string };
@@ -95,7 +95,8 @@ function EventCarousel({ id, slides }: Props) {
                 style={{
                     height: 'min(56.25vw, 80dvh)'
                 }}
-                className="relative w-full mx-auto bg-primary-50 mt-7">
+                className="relative w-full mx-auto bg-primary-50 mt-7"
+            >
                 <div
                     className="pointer-events-none absolute top-0 left-0 w-full h-full rounded-md bg-transparent z-10"
                     style={{
@@ -108,6 +109,7 @@ function EventCarousel({ id, slides }: Props) {
                     }}
                 >
                 </div>
+
                 <div className="absolute -bottom-4 lg:-bottom-5 left-1/2 -translate-x-1/2 flex space-x-2 z-20">
                     {slides.map((_, index) => {
                         const realIndex = index + 1;
@@ -124,6 +126,10 @@ function EventCarousel({ id, slides }: Props) {
                         );
                     })}
                 </div>
+
+                <button onClick={goToNext} className=' w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12  text-white/70  absolute top-1/2 -right-5 sm:-right-7 md:-right-8 lg:-right-10 -translate-y-1/2 rounded-full z-20'><ChevronRight className=' stroke-[2.5] w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12' /></button>
+                <button onClick={goToPrevious} className=' w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12  text-white/70  absolute top-1/2 -left-5 sm:-left-7 md:-left-8 lg:-left-10 -translate-y-1/2 rounded-full z-20'><ChevronLeft className=' stroke-[2.5] w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12' /></button>
+
                 <div className="h-full overflow-hidden rounded-md">
                     <div
                         className="flex h-full"
@@ -134,15 +140,28 @@ function EventCarousel({ id, slides }: Props) {
                         onTransitionEnd={handleTransitionEnd}
                     >
                         {extendedSlides.map((slide, index) => (
-                            <div key={index} className="relative w-full flex-shrink-0 h-full">
+                            <div
+                                key={index}
+                                className="relative w-full flex-shrink-0 h-full"
+                                onClick={() => {
+                                    const selection = window.getSelection();
+                                    const isSelecting = selection && selection.toString().length > 0;
+                                    if (isSelecting) {
+                                        return
+                                    }
+
+                                    toggleExpanded(index);
+                                }}
+                            >
                                 <img src={slide.image.src} alt={slide.caption} className="w-full h-full object-cover" />
-                                <div style={{
+
+                                {/* <div style={{
                                     clipPath: 'polygon(100% 0, 100% 100%, 0 100%)'
                                 }} className='bg-accent-300 absolute bottom-0 right-0 h-10 w-12 z-30'>
                                     <a target='_blank' href={slide.link}>
                                         <SquareArrowOutUpRight className='inline-block ml-7 mt-3.5 w-4' />
                                     </a>
-                                </div>
+                                </div> */}
 
                                 <div
                                     style={{
@@ -154,11 +173,11 @@ function EventCarousel({ id, slides }: Props) {
                                         }`}
                                 />
 
-                                <div onClick={() => toggleExpanded(index)} className={` overflow-visible p-4 pt-3 absolute top-0 left-0 z-30 rounded-br-md transition-all duration-300 ${expandedSlide === index ? 'h-[100%] w-[100%] backdrop-blur-xs' : 'h-[30%] w-[55%]'}`}>
+                                <div className={` overflow-visible p-4 pt-3 absolute top-0 left-0 z-30 rounded-br-md transition-all duration-300 ${expandedSlide === index ? 'h-[100%] w-[70%] backdrop-blur-xs' : 'h-[30%] w-[55%]'}`}>
 
                                     <div className="">
-                                        <h2 onClick={() => toggleExpanded(index)} className=' cursor-pointer text-white font-normal text-xs sm:text-base md:text-lg lg:text-xl'>{slide.caption}</h2>
-                                        <p className='text-slate-300 font-normal text-[0.625rem] sm:text-sm md:text-base lg:text-lg mt-1'>
+                                        <h2 className='  text-white font-normal text-xs sm:text-base md:text-lg lg:text-xl'>{slide.caption}</h2>
+                                        <p className='text-slate-300 font-normal text-2xs sm:text-sm md:text-base lg:text-lg mt-1'>
                                             <Calendar className='inline-block mb-0.5 mr-1 w-3 md:w-4 md:mb-1' />
                                             {slide.date.toISOString().split('T')[0]}
                                         </p>
@@ -168,20 +187,20 @@ function EventCarousel({ id, slides }: Props) {
                                         <div className="mt-1 md:mt-3 lg:mt-4 animate-in fade-in-0 slide-in-from-top-2 duration-300">
                                             <div className="space-y-3">
                                                 <div>
-                                                    <p className="text-white/90 font-medium text-xs md:text-base mb-0 md:mb-1">主辦單位</p>
-                                                    <p className="text-slate-300 text-[0.625rem] md:text-sm">{slide.organizer}</p>
+                                                    <p className="text-white/90 font-medium text-xs md:text-base lg:text-lg mb-0 md:mb-1">主辦單位</p>
+                                                    <p className="text-slate-300 text-2xs md:text-sm lg:text-base ">{slide.organizer}</p>
                                                 </div>
                                                 <div>
-                                                    <p className="text-white/90 font-medium text-xs md:text-base mb-0 md:mb-1">活動說明</p>
-                                                    <p className="text-slate-300 text-[0.625rem] md:text-sm leading-relaxed">{slide.description}</p>
+                                                    <p className="text-white/90 font-medium text-xs md:text-base lg:text-lg mb-0 md:mb-1">活動說明</p>
+                                                    <p className="text-slate-300 text-2xs md:text-sm lg:text-base  leading-relaxed">{slide.description}</p>
                                                 </div>
-                                                <div className="pt-2">
+                                                <div className="">
                                                     <a
                                                         href={slide.link}
                                                         target="_blank"
-                                                        className="inline-flex items-center text-accent-300 hover:text-accent-400 transition-colors text-sm md:text-base"
+                                                        className="inline-flex items-center text-accent-300 hover:text-accent-400 transition-colors text-sm md:text-base lg:text-lg "
                                                     >
-                                                        了解更多 <SquareArrowOutUpRight className="ml-1 w-3 h-3" />
+                                                        了解更多 <SquareArrowOutUpRight className="ml-1 w-4 inline-block mt-0.5" />
                                                     </a>
                                                 </div>
                                             </div>
@@ -192,9 +211,6 @@ function EventCarousel({ id, slides }: Props) {
                         ))}
                     </div>
                 </div>
-
-                <button onClick={goToPrevious} className="text-base md:text-xl lg:text-2xl p-1 md:p-2 pb-2 md:pb-3 absolute top-1/2 -left-2 lg:-left-4 -translate-y-1/2 bg-white/20 rounded-full backdrop-blur-xs z-30">‹</button>
-                <button onClick={goToNext} className="text-base md:text-xl lg:text-2xl p-1 md:p-2 pb-2 md:pb-3 absolute top-1/2 -right-2 lg:-right-4 -translate-y-1/2 bg-white/20 rounded-full backdrop-blur-xs z-30">›</button>
             </div>
         </div>
     );
