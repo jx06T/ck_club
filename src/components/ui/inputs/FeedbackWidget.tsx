bd34c585ef10d6522655fc31124bb61dcb7235cd
jx06t <50313tjx06@gmail.com>
2025-07-18 20:39:46 +0800

    feedback

--- src/components/ui/inputs/FeedbackWidget.tsx
+++ src/components/ui/inputs/FeedbackWidget.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, type FormEvent } from 'react';
 import { MessageSquare, X, Send, Loader, CheckCircle, AlertTriangle } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
-const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzXDNI4fJYjE6IgEtFUtAt6jXB3J3nwij7Pd_eqkmAtB7NVLPxdylpT-7pRBy5lmSI-/exec';
+const GAS_WEB_APP_URL = process.env.FEEDBACK_URL||'https://script.google.com/macros/s/AKfycbzLDsCMel79qbRmU1Q0JosjghVhtRZOeAXgJ1abV2d6s9TzEJzk0BR5j30jvtxqFCjJ/exec';
 
 const FEEDBACK_TYPES = ["內文更正", "Bug 回報", "改善建議", "其他"];
 
